20711ea05b2ab1852375211b3b76e3773c3d4969
venugopalvsv <62465276+venugopalvsv@users.noreply.github.com>
2020-05-17 14:11:47 +0800

    Revert "SeleniumCucumberProject1 Enabled two Features to run"
    This reverts commit 08d70bac0bc3f97568a53fc1e1f44fc9180865e9.

--- SeleniumCucumberProject1/src/test/java/testRunner/TestRun.java
+++ SeleniumCucumberProject1/src/test/java/testRunner/TestRun.java
@@ -8,8 +8,8 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 //		features=".//Features/",  // to run all feature files 
-//		features=".//Features/Customers.feature", // to run a single feature file
-		features={".//Features/Customers.feature",".//Features/Login.feature"}, // to run multiple/selected feature files 
+		features=".//Features/Customers.feature", // to run a single feature file
+//		features={".//Features/Customers.feature",".//Features/Login.feature"}, // to run multiple/selected feature files 
 		glue="stepDefinitions", // same applicable for multiple step definitions
 		dryRun=false, //it will cross check whether for every feature file step, methods are implemented of not 
 // Make dryrun=false when u want to execute the actual script
